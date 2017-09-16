d4c2dc4aae1261bce62b5c6cf8cbc4d832dc6fbe
gewenyu99 <wenyu1999@hotmail.com>
2017-09-16 14:52:07 -0400

    Added more visual elements

--- MrGoose/src/App.js
+++ MrGoose/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Image from './Image';
 import mleaf from './Maple_Leaf.svg'
+import god from './god.png'
 
 class App extends Component {
 
@@ -27,22 +28,28 @@ class App extends Component {
 
   render() {
     const style1 = {
+      marginTop: '0',
       position: 'absolute',
       left: ''+0.3*this.state.width,
       top: ''+0.01*this.state.width
     }
     const style21 = {
-
-      textAlign: 'center',
+      marginTop: '0',
+      position:'absolute',
+      left: ''+0.36*this.state.width,
+      top: ''+0.003*this.state.width,
       display: 'inline-block',
+
       padding: '0',
       marginTop: '0',
       marginRight: ''+ 0.03*this.state.width,
       fontSize: ''+0.06*this.state.width
     }
     const style22 = {
+      position:'absolute',
+      left: ''+0.47*this.state.width,
+      top: ''+0.003*this.state.width,
 
-      textAlign: 'center',
       display: 'inline-block',
       padding: '0',
       marginTop: '0',
@@ -50,39 +57,69 @@ class App extends Component {
       fontSize: ''+0.06*this.state.width
     }
     const style3 = {
+      marginTop: '0',
       position: 'absolute',
       left: ''+0.65*this.state.width,
       top: ''+0.01*this.state.width
     }
 
     const style4 = {
+      marginTop: '0',
       display: 'inline-block',
       position: 'absolute',
       left: ''+0.455*this.state.width,
       top: ''+0.025*this.state.width
     }
 
+    const style5 = {
+      marginTop: '0',  
+      display: 'float',
+      position: 'absolute',
+      left: '0px',
+      top: '0px'
+    }
+
+    const god1CSS = {
+      marginTop: '0',
+      display: 'inline-block',
+      position: 'absolute',
+      left: ''+0.12*this.state.width,
+      top: '0'
+    }
+
+    const god2CSS = {
+      marginTop: '0',
+      display: 'inline-block',
+      position: 'absolute',
+      left: ''+0.7  *this.state.width,
+      top: '0'
+    }
+
     return (
-      <div className="App">
-        <div className='TitleBar'>
-          <svg style = {style1} id= 'topleft'>
-            <rect width={this.state.width*0.05} height={this.state.width*0.07} x="0" y="0" fill='#D91E18'/>
-          </svg>
-          <h1 style = {style21}>
-            Mr
-          </h1>
-          <div style={style4}>
-            <img src={mleaf} width={0.05*this.state.width} />
+      <div>
+        <div className="App">
+          <div className='TitleBar'>
+            <img src = {god} width={this.state.width*0.2} style={god1CSS}/>
+            <img src = {god} width={this.state.width*0.2} style={god2CSS} className = 'godReverse'/>
+            <svg style = {style1} id= 'topleft'>
+              <rect width={this.state.width*0.05} height={this.state.width*0.07} x="0" y="0" fill='#D91E18'/>
+            </svg>
+            <h1 style = {style21}>
+              Mr
+            </h1>
+            <div style={style4}>
+              <img src={mleaf} width={0.05*this.state.width} />
+            </div>
+            <h1 style = {style22}>
+              Goose
+            </h1>
+            <svg style = {style3}>
+              <rect width={this.state.width*0.05} height={this.state.width*0.07} x="0" y="0" fill='#D91E18'/>
+            </svg>
           </div>
-          <h1 style = {style22}>
-            Goose
-          </h1>
-          <svg style = {style3}>
-            <rect width={this.state.width*0.05} height={this.state.width*0.07} x="0" y="0" fill='#D91E18'/>
-          </svg>
+          
+          <Image/>
         </div>
-        
-        <Image/>
       </div>
     );
   }
